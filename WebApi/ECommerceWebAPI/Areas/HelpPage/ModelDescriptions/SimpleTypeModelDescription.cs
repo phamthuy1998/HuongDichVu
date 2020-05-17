6335bc8fc161f09373dc9f384536cdee573489eb
quanjoker259 <50254969+quanjoker259@users.noreply.github.com>
2020-05-17 17:08:08 +0700

    remove project

--- WebApi/ECommerceWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ECommerceWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- WebApi/ECommerceWebAPI/obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECommerceWebAPI.WebApiApplication" Language="C#" %>
