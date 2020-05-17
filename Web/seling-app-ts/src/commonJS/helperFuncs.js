ae79cc5974b6038b96518ee4929670b409cc8051
Lê Thu <thukara210398@gmail.com>
2020-05-17 13:38:01 +0700

    Auto stash before rebase of "origin/master"

---
+++ Web/seling-app-ts/src/commonJS/helperFuncs.js
@@ -0,0 +1,20 @@
+export const isEmpty = value =>
+
+    value === undefined ||
+
+    value === null ||
+
+    (typeof value === "object" && Object.keys(value).length === 0) ||
+
+    (typeof value === "string" && value.trim().length === 0);
+
+export const subString = (text, maxLength = 50) => {
+    if (!isEmpty(text)) {
+        let len = text.length;
+
+        if (len <= maxLength) {
+            return text;
+        }
+        return text.substr(0, maxLength) + "...";
+    }
+}
